f1936346589a3b80b7e2866b308b34ecbf5d55b6
Edson <edbdelima@gmail.com>
2023-06-17 11:11:45 -0300

    Including map routes GET, POST, PUT, DELETE

--- Program.cs
+++ Program.cs
@@ -23,10 +23,26 @@ app.UseSwaggerUI(c => {
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+app.MapGet("/pizza/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsync(id));
 app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) => {
     await db.Pizzas.AddAsync(pizza);
     await db.SaveChangesAsync();
     return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
+app.MapPut("/pizza{id}", async (PizzaDb db, Pizza pizza, int id) => {
+    var pizzaToUpdate = await db.Pizzas.FindAsync(id);
+    if (pizzaToUpdate is null) return Results.NotFound();
+    pizzaToUpdate.Name = pizza.Name;
+    pizzaToUpdate.Description = pizza.Description;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+app.MapDelete("/pizza/{id}", async (PizzaDb db, int id) => {
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null) return Results.NotFound();
+    db.Pizzas.Remove(pizza);
+    await db.SaveChangesAsync();
+    return Results.Ok();
 });
 
 app.Run();
